fddacd289d93ef8497047737f5e5e76a8b48164a
iwonaive <szymaniak.iwona11@gmail.com>
2025-11-16 16:54:27 +0100

    add list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { InputButtonUnit } from './input-button-unit/input-button-unit';
+import { TodoItem } from "./todo-item/todo-item";
 
 @Component({
   selector: 'app-root',
-  imports: [InputButtonUnit],
+  imports: [InputButtonUnit, TodoItem],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
