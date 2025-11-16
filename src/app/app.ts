ea205e58154ce861d2026fee26d7deae31497e78
iwonaive <szymaniak.iwona11@gmail.com>
2025-11-16 16:41:31 +0100

    last changes for today

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,13 @@ import { InputButtonUnit } from './input-button-unit/input-button-unit';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('todo-list');
+  title = 'todo-list';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 }
